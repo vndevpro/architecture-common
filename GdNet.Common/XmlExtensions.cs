88c8d9f2f90597e8306ecda251cd39f10529b88c
juanonsoftware <juanonsoftware@github.com>
2018-08-21 15:02:28 +0700

    Update XmlExtensions to add new ToXmlIgnoreNullOrEmpty method

--- GdNet.Common/XmlExtensions.cs
+++ GdNet.Common/XmlExtensions.cs
@@ -10,15 +10,29 @@ namespace GdNet.Common
     public static class XmlExtensions
     {
         /// <summary>
-        /// Returns xml document for the dictionary, root element is items and child element is item
+        /// Returns a xml document for the dictionary, root element is items and child elements are item
         /// </summary>
         /// <param name="data"></param>
         /// <returns></returns>
         public static string ToXml(this IDictionary<string, string> data)
         {
-            var xElem = new XElement("items",
-                data.Select(x => new XElement("item", new XAttribute("key", x.Key), new XAttribute("value", x.Value)))
-            );
+            var xmlContent = data.Select(x => new XElement("item", new XAttribute("key", x.Key), new XAttribute("value", x.Value)));
+            var xElem = new XElement("items", xmlContent);
+            return xElem.ToString();
+        }
+
+        /// <summary>
+        /// Returns a xml document for the dictionary, root element is items and child elements are item (not containing any null or empty value on key/value)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string ToXmlIgnoreNullOrEmpty(this IDictionary<string, string> data)
+        {
+            var xmlContent = data
+                .Where(x => !string.IsNullOrEmpty(x.Key) && !string.IsNullOrEmpty(x.Value))
+                .Select(x => new XElement("item", new XAttribute("key", x.Key), new XAttribute("value", x.Value)));
+
+            var xElem = new XElement("items", xmlContent);
             return xElem.ToString();
         }
 
