34539a9ce2978605fed903adad9e075dd35e04d6
juanonsoftware <juanonsoftware@github.com>
2018-09-07 11:47:43 +0700

    V2.0.0

---
+++ GdNet.Common/Entities/DynPropertyEntityBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace GdNet.Common.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class DynPropertyEntityBase : IDynPropertyEntity
+    {
+        /// <summary>
+        /// Dictionary contains all dyn properties
+        /// </summary>
+        protected IDictionary<string, string> _metadata;
+
+        protected DynPropertyEntityBase()
+        {
+            _metadata = new Dictionary<string, string>();
+        }
+
+        public string MetadataXml
+        {
+            get
+            {
+                return _metadata.ToXmlIgnoreNullOrEmpty();
+            }
+            set
+            {
+                _metadata = XmlExtensions.ParseXml(value);
+            }
+        }
+    }
+}
---
+++ GdNet.Common/Entities/IDynPropertyEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace GdNet.Common.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDynPropertyEntity
+    {
+        /// <summary>
+        /// Xml data for all dyn properties
+        /// </summary>
+        string MetadataXml { get; set; }
+    }
+}
