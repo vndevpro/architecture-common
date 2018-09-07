c92a13912037cf5cc28d6fa6953bfe88b9846f03
juanonsoftware <juanonsoftware@github.com>
2018-09-07 10:52:45 +0700

    Adding Dictionary Extensions

---
+++ GdNet.Common/DictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GdNet.Common
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrSet(this IDictionary<string, string> dictionary, string key, string value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+
+        public static string GetOrDefault(this IDictionary<string, string> dictionary, string key)
+        {
+            return dictionary.GetOrDefault(key, default(string));
+        }
+
+        public static string GetOrDefault(this IDictionary<string, string> dictionary, string key, string @default)
+        {
+            return dictionary.ContainsKey(key) ? dictionary[key] : @default;
+        }
+    }
+}
