8cfd5922447d5b308db685bbbac960398dac79b8
juanonsoftware <juanonsoftware@github.com>
2018-09-19 15:39:47 +0700

    Add ObjectExtensions and new methods to DynPropertyEntityBase

---
+++ GdNet.Common/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace GdNet.Common
+{
+    /// <summary>
+    /// Extension methods for Object
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Convert an anonymous object to dynamic object so that it's possible to use on other namespaces (eg on cshtml)
+        /// </summary>
+        public static dynamic ToDynamic(this object anonymousObject)
+        {
+            IDictionary<string, object> expando = new ExpandoObject();
+
+            foreach (PropertyDescriptor propertyDescriptor in TypeDescriptor.GetProperties(anonymousObject))
+            {
+                var obj = propertyDescriptor.GetValue(anonymousObject);
+                expando.Add(propertyDescriptor.Name, obj);
+            }
+
+            return expando;
+        }
+    }
+}
---
+++ GdNet.Common/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace GdNet.Common
+{
+    /// <summary>
+    /// Extension methods for String
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Trim a string
+        /// </summary>
+        public static string TrimSafe(this string input)
+        {
+            return (input == null) ? null : input.Trim();
+        }
+    }
+}
